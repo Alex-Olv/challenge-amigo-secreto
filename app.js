308479b64441b7bcd624b263a98f572001581114
alex <alexoliveiracurvo@gmail.com>
2025-02-01 19:52:29 -0300

    criando a logica com JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos= [];
+
+function adicionarAmigo() {
+   let nomeDoAmigo = document.querySelector("input").value;
+    if (nomeDoAmigo == "") {
+        alert('Por favor, insira um nome valido');
+        return;
+    }
+    amigos.push(nomeDoAmigo);
+    atualizarLista();
+    limparCampo();
+    }
+
+function limparCampo() {
+   let amigos = document.querySelector('input');
+    amigos.value = '';
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    let nomes = ''; 
+    for (let i = 0; i < amigos.length; i++) {
+        nomes += `<li>${amigos[i]}</li>`;
+    }
+    lista.innerHTML = nomes; 
+}
+
+function sortearAmigo() {
+   if (amigos.length < 2) {
+    alert('Digite pelo menos dois nomes para sortear!');   
+    } else {
+        let indiceSorteado = Math.floor(Math.random() * amigos.length);
+        nomeSorteado = amigos[indiceSorteado];
+        resultado.innerHTML= `O amigo secreto é ${nomeSorteado}`;
+    }
+   }
